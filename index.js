e807dc727666c980e4b681935fa046b24dd074d3
danielgallo <daniel.gallo@gmail.com>
2021-02-21 17:18:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,24 +36,25 @@ request(url, function (error, response, body) {
     } else {
         let weather = JSON.parse(body);
 
-        var text = '';
-
-        text += `Outside Temperature: ${weather.main.temp.toFixed(1)}C \n`;
-        text += `Feels Like: ${weather.main.feels_like.toFixed(1)}C\n`;
-        text += `Humidity: ${weather.main.humidity}%\n`;
-        text += `Visibility: ${weather.visibility / 1000} km\n`
+        var text = '',
+            windSpeed = 0,
+            windDirection = 'n/a',
+            windGust = 0,
+            rain1 = 0,
+            rain3 = 0;
 
         if (weather.wind) {
-            text += `Wind Speed: ${(weather.wind.speed * 3.6).toFixed(0)} km/h\n`
-            text += `Wind Direction: ${compass.cardinalFromDegree(weather.wind.deg)}\n`;
-        }
+            windSpeed = weather.wind.speed;
+            windDirection = weather.wind.deg;
 
-        if (weather.wind.gust) {
-            text += `Wind Gust: ${(weather.wind.gust * 3.6).toFixed(0)} km/h\n`;
+            if (weather.wind.gust) {
+                windGust = weather.wind.gust;
+            }
         }
 
         if (weather.rain) {
-            text += `Rain (Past Hour): ${weather.rain["1h"]} mm\n`;
+            rain1 = weather.rain["1h"].toFixed(1);
+            rain3 = weather.rain["3h"].toFixed(1);
         }
 
         let sunriseUtc = new Date(weather.sys.sunrise * 1000);
@@ -61,8 +62,17 @@ request(url, function (error, response, body) {
         let sunsetUtc = new Date(weather.sys.sunset * 1000);
         let sunsetLocal = utcToZonedTime(sunsetUtc, argv.region);
 
-        text += `Sunrise: ${format(sunriseLocal, 'HH:mm', { timeZone: argv.region })}am\n`;
-        text += `Sunset: ${format(sunsetLocal, 'HH:mm', { timeZone: argv.region })}pm\n`;
+        text += `Outside Temperature: ${weather.main.temp.toFixed(1)}C \n`;
+        text += `Feels Like: ${weather.main.feels_like.toFixed(1)}C\n`;
+        text += `Humidity: ${weather.main.humidity}%\n`;
+        text += `Visibility: ${weather.visibility / 1000} km\n`;
+        text += `Wind Speed: ${(windSpeed * 3.6).toFixed(0)} km/h\n`;
+        text += `Wind Direction: ${compass.cardinalFromDegree(windDirection)}\n`;
+        text += `Wind Gust: ${(windGust * 3.6).toFixed(0)} km/h\n`;
+        text += `Rain (Last 1 Hour): ${rain1} mm\n`;
+        text += `Rain (Last 3 Hours): ${rain3} mm\n`;
+        text += `Sunrise: ${format(sunriseLocal, 'HH:mm', { timeZone: argv.region })}\n`;
+        text += `Sunset: ${format(sunsetLocal, 'HH:mm', { timeZone: argv.region })}\n`;
 
         fs.outputFileSync(argv.output, text);
     }
